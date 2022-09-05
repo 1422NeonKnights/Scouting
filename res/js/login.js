b1137a2c6edd683308decf2a0dadb83451f3fba8
“Error1223” <hyunsu1129.lim@gmail.com>
2022-09-05 13:04:37 -0700

    redirect

--- res/js/login.js
+++ res/js/login.js
@@ -1,3 +1,8 @@
+const username = "frc1422"
+const password = "neonknightsrock"
+
+var autologin = true;
+
 function setFormMessage(formElement, type, message) {
     const messageElement = formElement.querySelector(".form__message");
 
@@ -14,12 +19,33 @@ function clearPasswordInput(){
     document.querySelector(".form__input-password").value= "";
 }
 
+function getCookie(name) {
+    var cookieArr = document.cookie.split(";");
+  
+    for(var i = 0; i < cookieArr.length; i++) {
+        var cookiePair = cookieArr[i].split("=");
+        
+        if(name == cookiePair[0].trim()) {
+            // Decode the cookie value and return
+            return decodeURIComponent(cookiePair[1]);
+        }
+    }
+    return null;
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+    if(getCookie("autologin") != ""){
+        location.href = "utility.html";
+    }
+});
+
 document.querySelector(".form__button").addEventListener("click", e => {
     e.preventDefault();
     const usrnameInput = document.querySelector(".form__input-username").value;
     const passwordInput = document.querySelector(".form__input-password").value;
 
-    if(usrnameInput == "frc1422" && passwordInput == "neonknightsrock"){
+    if(usrnameInput == username && passwordInput == password){
+        document.cookie = "autologin=true;";
         location.href = "utility.html";
     }else{
         setFormMessage(document, "error",  "Incorrect username/password");
